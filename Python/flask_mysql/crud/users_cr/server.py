d3475cd7f3b8e81d950ec2dd3dc3eeb7230bbc3c
Chandler McAdory <chandlermcadory@gmail.com>
2022-11-04 10:59:47 -0400

    crud updates

--- Python/flask_mysql/crud/users_cr/server.py
+++ Python/flask_mysql/crud/users_cr/server.py
@@ -11,15 +11,35 @@ def homepage():
     all_users = User.get_all()
     return render_template('read_all.html', all_users = all_users)
 
-
 @app.route('/users/new')
 def new():
     return render_template('create.html')
 
-
 @app.route('/users/create', methods = ['POST'])
 def create_user():
     User.create(request.form)
+    return redirect('/')
+
+@app.route('/users/<int:id>/edit')
+def edit_user(id):
+    return render_template("edit.html",user=User.get_one({'id':id}))
+
+@app.route('/users/<int:id>/update',methods=['POST'])
+def update(id):
+    data = {
+        **request.form,
+        'id' : id
+    }
+    User.update(data)
+    return redirect('/')
+
+@app.route('/users/<int:id>/show')
+def show(id):
+    return render_template("show_user.html",user=User.get_one({'id':id}))
+
+@app.route('/users/<int:id>/destroy')
+def delete_user(id):
+    User.delete({"id" : id})
     return redirect('/')
 
 
--- Python/flask_mysql/crud/users_cr/user_model.py
+++ Python/flask_mysql/crud/users_cr/user_model.py
@@ -13,7 +13,9 @@ class User:
 
     @classmethod 
     def get_all(cls):
-        query = 'SELECT * FROM users;'
+        query = '''
+        SELECT * FROM users;
+        '''
         results = connectToMySQL(DATABASE).query_db(query)
         print(results)
         all_users = []
@@ -24,9 +26,35 @@ class User:
         return all_users
 
     @classmethod
+    def get_one(cls,data):
+        query = '''
+        SELECT * FROM users WHERE id = (%(id)s);
+        '''
+        result = connectToMySQL('users_schema').query_db(query,data)
+        return cls(result[0])
+
+    @classmethod
     def create(cls, data):
         query = '''
         INSERT INTO users(first_name, last_name, email)
-        VALUES (%(first_name)s, %(last_name)s, %(email)s)
+        VALUES (%(first_name)s, %(last_name)s, %(email)s);
         '''
         return connectToMySQL(DATABASE).query_db(query, data)
+        
+    @classmethod
+    def update(cls, data):
+        query = '''
+        UPDATE users
+        SET first_name=%(first_name)s,last_name=%(last_name)s,email=%(email)s
+        WHERE id = (%(id)s);
+        '''
+        return connectToMySQL(DATABASE).query_db(query, data)
+
+    @classmethod
+    def delete(cls,data):
+        query = '''
+        DELETE FROM users WHERE id = (%(id)s);
+        '''
+        return connectToMySQL(DATABASE).query_db(query, data)
+
+    
