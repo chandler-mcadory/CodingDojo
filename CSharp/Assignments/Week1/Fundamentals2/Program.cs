20d26bb11b973fe08b81e5b3ce9273ec7ad0281b
Chandler McAdory <chandlermcadory@gmail.com>
2022-11-30 17:22:04 -0500

    practice

---
+++ CSharp/Assignments/Week1/Fundamentals2/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Hello, World!");
+
+
+// Three Basic Arrays
+
+// int[] numbers = new int[10];
+// for(int i = 0; i < numbers.Length; i++)
+// {
+//     numbers[i] = i;
+//     Console.WriteLine(numbers[i]);
+// }
+
+// string[] names = new string[] { "Tim", "Martin", "Nikki", "Sara"};
+// for(int i = 0; i < names.Length; i++)
+// {
+//     Console.WriteLine(names[i]);
+// }
+
+// bool[] tfs = new bool[10];
+// for(int i = 0; i < tfs.Length; i++)
+// {
+//     tfs[i] = i % 2 == 0;
+//     Console.WriteLine(tfs[i]);
+// }
+
+
+// List of Flavors
+
+List<string> flavors = new List<string>();
+flavors.Add("Chocolate");
+flavors.Add("Vanilla");
+flavors.Add("Cookie Dough");
+flavors.Add("Cookies and Cream");
+flavors.Add("Fudge Brownie");
